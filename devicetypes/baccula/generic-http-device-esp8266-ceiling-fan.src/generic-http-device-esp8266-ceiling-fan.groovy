8575fc6c17aa5af44244a9a21b9d85096b97b4f3
Mike Neuwirth <baccula@gmail.com>
2016-11-22 18:10:17 -0700

    Merged Photon code and Z-Wave Switch Code
    TODO: Z-Wave control broken after ESP8266 Action

--- devicetypes/baccula/generic-http-device-esp8266-ceiling-fan.src/generic-http-device-esp8266-ceiling-fan.groovy
+++ devicetypes/baccula/generic-http-device-esp8266-ceiling-fan.src/generic-http-device-esp8266-ceiling-fan.groovy
@@ -19,18 +19,19 @@
  *
  */
 
-import groovy.json.JsonSlurper
-
 metadata {
 	definition (name: "Generic HTTP Device - ESP8266 - Ceiling Fan", author: "baccula", namespace:"baccula") {
-		attribute "triggerswitch", "string"
-		attribute "hispeed", "string"
-        attribute "medspeed", "string"
-        attribute "lospeed", "string"
-		command "DeviceTrigger"
-		command "speedHiTrigger"
-        command "speedMedTrigger"
-        command "speedLoTrigger"
+		capability "Switch"
+		capability "Switch Level"
+        capability "Refresh"
+
+		command "fanOff"
+        command "lowSpeed"
+        command "medSpeed"
+        command "highSpeed"
+        command "push"
+        
+        attribute "currentSpeed", "string"
 	}
 
 
@@ -39,56 +40,165 @@ metadata {
 		input("DevicePort", "string", title:"Device Port", description: "Please enter port 80 or your device's Port", required: true, displayDuringSetup: true)
 		input("DevicePath", "string", title:"URL Path", description: "Rest of the URL, include forward slash.", displayDuringSetup: true)
 		input(name: "DevicePostGet", type: "enum", title: "POST or GET", options: ["POST","GET"], required: true, displayDuringSetup: true)
-		input("DeviceBodyText", "string", title:'Body Content', description: 'Type in "GateTrigger=" or "CustomTrigger="', required: true, displayDuringSetup: true)
-		input("UseJSON", "bool", title:"Use JSON instead of HTML?", description: "Use JSON instead of HTML?", defaultValue: false, required: false, displayDuringSetup: true)
+		//input("DeviceBodyText", "string", title:'Body Content', description: 'Type in "GateTrigger=" or "CustomTrigger="', required: true, displayDuringSetup: true)
 	}
 
 	simulator {
 	}
 
 	tiles {
-		standardTile("DeviceTrigger", "device.triggerswitch", width: 2, height: 2, canChangeIcon: true, canChangeBackground: true) {
-			state "triggerswitch", label:'Light' , action: "triggerswitch", icon: "st.Appliances.appliances11", backgroundColor:"#ffffff", nextState: "trying"
-			state "trying", label: 'TRYING', action: "", icon: "st.Appliances.appliances11", backgroundColor: "#FFAA33"
+        standardTile("switch", "device.switch", width: 2, height: 2, canChangeIcon: true) {
+			state "off", label: '${name}', action: "switch.on", icon:"st.Lighting.light24", backgroundColor: "#ffffff", nextState: "on"
+			state "on", label: '${name}', action: "switch.off", icon:"st.Lighting.light24", backgroundColor: "#79b821"    
+
 		}
-		standardTile("speedHiTrigger", "device.speedhi", width: 1, height: 1, canChangeIcon: true, canChangeBackground: true, decoration: "flat") {
-			state "default", label:'Speed Hi' , action: "speedHiTrigger", icon: "st.Weather.weather1", backgroundColor:"#ff4c4c", nextState: "trying"
-			state "trying", label: 'TRYING', action: "", icon: "st.Weather.weather1", backgroundColor: "#FFAA33"
+        //Slider not show in display but kept in for trouble shooting / testing, if needed. 
+		controlTile("levelSliderControl", "device.level", "slider", height: 1, width: 3, inactiveLabel: false) {
+			state "level", action:"switch level.setLevel"
 		}
-        standardTile("speedMedTrigger", "device.speedmed", width: 1, height: 1, canChangeIcon: true, canChangeBackground: true, decoration: "flat") {
-			state "default", label:'Speed Med' , action: "speedMedTrigger", icon: "st.Weather.weather1", backgroundColor:"#ff4c4c", nextState: "trying"
-			state "trying", label: 'TRYING', action: "", icon: "st.Weather.weather1", backgroundColor: "#FFFF00"
+        
+        //displays current speed as off, low, med, high
+        valueTile("currentSpeed", "device.currentSpeed", canChangeIcon: false, inactiveLabel: false, decoration: "flat") {
+            state ("default", label:'${currentValue}')
+        }
+
+		//Speed control row
+        standardTile("fanOff", "device.level", inactiveLabel: false, decoration: "flat") {
+            state "fanOff", label:'OFF', action:"fanOff", icon:"st.Home.home30"
+        }
+        standardTile("lowSpeed", "device.level", inactiveLabel: false, decoration: "flat") {
+            state "lowSpeed", label:'LOW', action:"lowSpeed", icon:"st.Home.home30"
+        }
+        standardTile("medSpeed", "device.level", inactiveLabel: false, decoration: "flat") {
+            state "medSpeed", label:'MED', action:"medSpeed", icon:"st.Home.home30"
+        }
+        standardTile("highSpeed", "device.level", inactiveLabel: false, decoration: "flat") {
+            state "highSpeed", label:'HIGH', action:"highSpeed", icon:"st.Home.home30"
+        }
+        standardTile("refresh", "device.switch", inactiveLabel: false, decoration: "flat") {
+			state "default", label:"", action:"refresh.refresh", icon:"st.secondary.refresh"
 		}
-        standardTile("speedLoTrigger", "device.speedlo", width: 1, height: 1, canChangeIcon: true, canChangeBackground: true, decoration: "flat") {
-			state "default", label:'Speed Lo' , action: "speedLoTrigger", icon: "st.Weather.weather1", backgroundColor:"#ffff00", nextState: "trying"
-			state "trying", label: 'TRYING', action: "", icon: "st.Weather.weather1", backgroundColor: "#00FF00"
-		}
-		main "DeviceTrigger"
-		details(["DeviceTrigger", "speedHiTrigger", "speedMedTrigger","speedLoTrigger"])
+		main(["switch"])
+		details(["switch", "refresh", "currentSpeed", "fanOff", "lowSpeed", "medSpeed", "highSpeed"])
 	}
 }
 
-def DeviceTrigger() {
-	log.debug "Triggered on!!!"
-	sendEvent(name: "triggerswitch", value: "triggeron", isStateChange: true)
-    state.fan = "triggerswitch";
-	runCmd("fan=triggerswitch")
+def updated(){
+		// Device-Watch simply pings if no device events received for 32min(checkInterval)
+		sendEvent(name: "checkInterval", value: 2 * 15 * 60 + 2 * 60, displayed: false, data: [protocol: "zwave", hubHardwareId: device.hub.hardwareID])
 }
-def speedHiTrigger() {
+
+def zwaveEvent(physicalgraph.zwave.commands.basicv1.BasicReport cmd) {
+	[name: "switch", value: cmd.value ? "on" : "off", type: "physical"]
+}
+
+def zwaveEvent(physicalgraph.zwave.commands.basicv1.BasicSet cmd) {
+	[name: "switch", value: cmd.value ? "on" : "off", type: "physical"]
+}
+
+def zwaveEvent(physicalgraph.zwave.commands.switchbinaryv1.SwitchBinaryReport cmd) {
+	[name: "switch", value: cmd.value ? "on" : "off", type: "digital"]
+}
+
+def zwaveEvent(physicalgraph.zwave.commands.configurationv1.ConfigurationReport cmd) {
+	def value = "when off"
+	if (cmd.configurationValue[0] == 1) {value = "when on"}
+	if (cmd.configurationValue[0] == 2) {value = "never"}
+	[name: "indicatorStatus", value: value, displayed: false]
+}
+
+def zwaveEvent(physicalgraph.zwave.commands.hailv1.Hail cmd) {
+	[name: "hail", value: "hail", descriptionText: "Switch button was pressed", displayed: false]
+}
+
+def zwaveEvent(physicalgraph.zwave.commands.manufacturerspecificv2.ManufacturerSpecificReport cmd) {
+	log.debug "manufacturerId:   ${cmd.manufacturerId}"
+	log.debug "manufacturerName: ${cmd.manufacturerName}"
+	log.debug "productId:        ${cmd.productId}"
+	log.debug "productTypeId:    ${cmd.productTypeId}"
+	def msr = String.format("%04X-%04X-%04X", cmd.manufacturerId, cmd.productTypeId, cmd.productId)
+	updateDataValue("MSR", msr)
+	updateDataValue("manufacturer", cmd.manufacturerName)
+	createEvent([descriptionText: "$device.displayName MSR: $msr", isStateChange: false])
+}
+
+
+def zwaveEvent(physicalgraph.zwave.Command cmd) {
+	// Handles all Z-Wave commands we aren't interested in
+	[:]
+}
+
+def fanOff() {
+	log.debug "Fan Off!!!"
+    sendEvent(name: "switch", value: "on", isStateChange: false, display: false)
+    sendEvent(name: "currentSpeed", value: "OFF" as String)
+	runCmd("fan=fanoff")
+}
+
+def on() {
+	log.debug "Switch On!!!"
+	sendEvent(name: "switch", value: "on", isStateChange: true, display: false)
+	delayBetween([
+		zwave.basicV1.basicSet(value: 0xFF).format(),
+		zwave.switchBinaryV1.switchBinaryGet().format()
+	])
+}
+
+def off() {
+	log.debug "Switch Off!!!"
+	sendEvent(name: "switch", value: "off", isStateChange: true, display: false)
+	delayBetween([
+		zwave.basicV1.basicSet(value: 0x00).format(),
+		zwave.switchBinaryV1.switchBinaryGet().format()
+	])
+}
+def highSpeed() {
 	log.debug "High Speed!!!"
-    sendEvent(name: "hispeed", value: "default", isStateChange: true)
+    sendEvent(name: "switch", value: "on", isStateChange: false, display: false)
+    sendEvent(name: "currentSpeed", value: "HIGH" as String)
 	runCmd("fan=speedhi")
 }
-def speedMedTrigger() {
+def medSpeed() {
 	log.debug "Medium Speed!!!"
-    sendEvent(name: "medspeed", value: "default", isStateChange: true)
+    sendEvent(name: "switch", value: "on", isStateChange: false, display: false)
+    sendEvent(name: "currentSpeed", value: "MED" as String)
 	runCmd("fan=speedmed")
 }
-def speedLoTrigger() {
+def lowSpeed() {
 	log.debug "Low Speed!!!"
-    sendEvent(name: "lospeed", value: "default", isStateChange: true)
+    sendEvent(name: "switch", value: "on", isStateChange: false, display: false)
+    sendEvent(name: "currentSpeed", value: "LOW" as String)
 	runCmd("fan=speedlo")
 }
+
+def setLevel(val){
+    log.info "setLevel $val"
+    sendEvent(name:"level",value:val)        											
+        if (val == 0){ 
+        	runCmd("fan=fanoff")
+        	sendEvent(name: "switch", value: "on", isStateChange: false, display: false)
+       		log.info "Fan off"
+  		  	sendEvent(name: "currentSpeed", value: "OFF" as String)
+   			}
+        if ((val >= 1) & (val <=30)) {
+        	runCmd("fan=speedlo")
+        	sendEvent(name: "switch", value: "on", isStateChange: true, display: false)
+			log.info "Fan low"
+    		sendEvent(name: "currentSpeed", value: "LOW" as String)
+       		}
+        if ((val >= 31) & (val <=60)) {
+        	runCmd("fan=speedmed")
+        	sendEvent(name: "switch", value: "on", isStateChange: true, display: false)
+        	log.info "Fan Med"
+    		sendEvent(name: "currentSpeed", value: "MED" as String)
+			}
+        if ((val >= 61) & (val <=100)) {
+        	runCmd("fan=speedhi")
+        	sendEvent(name: "switch", value: "on", isStateChange: true, display: false)
+        	log.info "Fan High"
+    		sendEvent(name: "currentSpeed", value: "HIGH" as String)
+        	}                                                  
+	}
 
 
 def runCmd(String varCommand) {
@@ -144,33 +254,39 @@ def runCmd(String varCommand) {
 }
 
 def parse(String description) {
-	//log.debug "Parsing '${description}'"
-	def whichTile = ''	
-	log.debug "state.fan " + state.fan
-	
-    if (state.fan == "on") {
-    	//sendEvent(name: "triggerswitch", value: "triggergon", isStateChange: true)
-        whichTile = 'mainon'
-    }
-    if (state.fan == "off") {
-    	//sendEvent(name: "triggerswitch", value: "triggergoff", isStateChange: true)
-        whichTile = 'mainoff'
-    }
-	
-    //RETURN BUTTONS TO CORRECT STATE
-	log.debug 'whichTile: ' + whichTile
-    switch (whichTile) {
-        case 'mainon':
-			def result = createEvent(name: "switch", value: "on", isStateChange: true)
-			return result
-        case 'mainoff':
-			def result = createEvent(name: "switch", value: "off", isStateChange: true)
-			return result
-        default:
-			def result = createEvent(name: "testswitch", value: "default", isStateChange: true)
-			//log.debug "testswitch returned ${result?.descriptionText}"
-			return result
-    }
+	def result = null
+	def cmd = zwave.parse(description, [0x20: 1, 0x70: 1])
+	if (cmd) {
+		result = createEvent(zwaveEvent(cmd))
+	}
+	if (result?.name == 'hail' && hubFirmwareLessThan("000.011.00602")) {
+		result = [result, response(zwave.basicV1.basicGet())]
+		log.debug "Was hailed: requesting state update"
+	} else {
+		log.debug "Parse returned ${result?.descriptionText}"
+	}
+	return result
+}
+
+def poll() {
+	delayBetween([
+		zwave.switchBinaryV1.switchBinaryGet().format(),
+		zwave.manufacturerSpecificV1.manufacturerSpecificGet().format()
+	])
+}
+
+/**
+  * PING is used by Device-Watch in attempt to reach the Device
+**/
+def ping() {
+		refresh()
+}
+
+def refresh() {
+	delayBetween([
+		zwave.switchBinaryV1.switchBinaryGet().format(),
+		zwave.manufacturerSpecificV1.manufacturerSpecificGet().format()
+	])
 }
 
 private String convertIPtoHex(ipAddress) {
